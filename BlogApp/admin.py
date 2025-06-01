2391c6775396546e344ffb6e81418e8650be5e78
MuhammedNisham <nishamovingal602@gmail.com>
2025-06-01 16:24:17 +0530

    edits

--- BlogApp/admin.py
+++ BlogApp/admin.py
@@ -4,5 +4,14 @@ from .models import Post
 
 # Register your models here.
 admin.site.register(Author)
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'author_username', 'updated_at', 'created_at')
+    list_filter = ('author',)
+    search_fields = ('title', 'content')
+    ordering = ('-updated_at',)
+
+    def author_username(self, obj):
+        return obj.author.username
+    author_username.short_description = 'AUTHOR USERNAME'
 
